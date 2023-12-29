39739749859bf0f8539fa3db54ab52179d5ccc57
Eeap <kdg97811@naver.com>
2023-12-29 14:43:41 +0900

    refactor: basic test code

--- src/main/java/sumin/example/DataStreamJob.java
+++ src/main/java/sumin/example/DataStreamJob.java
@@ -16,8 +16,10 @@
  * limitations under the License.
  */
 
-package org.example;
+package sumin.example;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -37,7 +39,22 @@ public class DataStreamJob {
 	public static void main(String[] args) throws Exception {
 		// Sets up the execution environment, which is the main entry point
 		// to building Flink applications.
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		final StreamExecutionEnvironment env =
+				StreamExecutionEnvironment.getExecutionEnvironment();
+
+		DataStream<Person> flintstones = env.fromElements(
+				new Person("Fred", 35),
+				new Person("Wilma", 35),
+				new Person("Pebbles", 2));
+
+		DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
+			@Override
+			public boolean filter(Person person) throws Exception {
+				return person.age >= 18;
+			}
+		});
+
+		adults.print();
 
 		/*
 		 * Here, you can start creating your execution plan for Flink.
@@ -61,5 +78,19 @@ public class DataStreamJob {
 
 		// Execute program, beginning computation.
 		env.execute("Flink Java API Skeleton");
+	}
+	public static class Person {
+		public String name;
+		public Integer age;
+		public Person() {}
+
+		public Person(String name, Integer age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String toString() {
+			return this.name.toString() + ": age " + this.age.toString();
+		}
 	}
 }
